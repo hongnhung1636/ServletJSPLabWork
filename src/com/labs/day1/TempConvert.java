eb11704ee6b10a61e66d45176833d7357b3765fe
Hong Le <honle@visa.com>
2016-08-02 09:17:51 -0700

    add day 1 lab

---
+++ src/com/labs/day1/TempConvert.java
@@ -0,0 +1,21 @@
+package com.labs.day1;
+
+import java.util.Scanner;
+/**
+ *  A program that converts Fahrenheit temperatures into Celsius. The program will prompt the
+ *  user to enter a Fahrenheit temperature on the console, and will print out the equivalent Celsius value.
+ *  The mathematical formula for converting a Fahrenheit temperature into a Celsius temperature is:
+ *  C = 5 x ( F – 32 ) / 9
+ */
+public class TempConvert {
+
+    public static void main(String[] args) {
+        System.out.print("Enter Fahrenheit temperatures: ");
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        double f = Double.valueOf(line);
+        float c =  5 * ( (float)f - 32 ) / 9;
+        System.out.println((double) c);
+    }
+
+}
